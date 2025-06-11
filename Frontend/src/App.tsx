a60c5e5b9aeb0920e12516da162fca46794c0eee
Vhmed <vhmdhoss@gmail.com>
2025-06-11 15:09:31 +0300

    adding random edit

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -4,10 +4,11 @@ export default function App() {
   return (
     <div className="p-2">
       <h2>Notreon Project on development!.</h2>
-      <p>Steps left for development:</p>
+      <p>Steps left for frontend:</p>
       <ul>
         <li>1. logo</li>
         <li>2. backend</li>
+        <li>3. images</li>
       </ul>
       <Button variant="outline" onClick={() => alert("NO!")}>Notify when ready</Button>
     </div>
