2b14913ea9539819598c3cece9707af0f585837b
Vhmed <vhmdhoss@gmail.com>
2025-06-13 02:06:54 +0300

    setting up imagekit and downloading multer

--- Backend/src/Lib/imageKit.js
+++ Backend/src/Lib/imageKit.js
@@ -1,16 +1,10 @@
-import {config} from 'dotenv';
-import ImageKit from 'imagekit';
-
+import { config } from "dotenv";
+import ImageKit from "imagekit";
 config();
 const imageKit = new ImageKit({
-    publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
-    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
-    privateKey: process.env.IMAGEKIT_PRIVATE_KEY
-})
-
-const url = imageKit.url({
-    path: "/sample-image.jpg",
-    transformation: [{ width: 300, format: "webp" }]
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
 });
 
-console.log(url);
+export default imageKit;
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import connectMongoDB from "./Lib/Mongodb.js";
-
 const app = express();
 dotenv.config();
 app.use(
