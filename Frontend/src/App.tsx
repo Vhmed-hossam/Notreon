316f6980c22e7f87fb9a85c10735c432e56a776b
Vhmed <vhmdhoss@gmail.com>
2025-06-12 20:28:33 +0300

    added logo and images

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -6,9 +6,7 @@ export default function App() {
       <h2>Notreon Project on development!.</h2>
       <p>Steps left for frontend:</p>
       <ul>
-        <li>1. logo</li>
         <li>2. backend</li>
-        <li>3. images</li>
       </ul>
       <Button variant="outline" onClick={() => alert("NO!")}>Notify when ready</Button>
     </div>
