a4b8d99b287e8c1f46e96be81de4cc71604d1f60
Vhmed <vhmdhoss@gmail.com>
2025-06-14 01:57:59 +0300

    more error messages

--- Backend/src/Messages/Errs/Errormsgs.js
+++ Backend/src/Messages/Errs/Errormsgs.js
@@ -2,5 +2,7 @@ export const ErrorMessages = {
   noToken: "Unauthorized - no token found",
   invalidToken: "Unauthorized - Invalid Token",
   userNotFound: "Unauthorized - User not found",
-  ISerror: "Internal Server Error"
+  ISerror: "Internal Server Error",
+  userAlreadyExists: "User already exists",
+  cannotcreateuser: "Cannot create user , invalid data"
 };
