c3a8176160fb946df2017c5cd4135c5e255d1a20
Vhmed <vhmdhoss@gmail.com>
2025-06-15 20:53:43 +0300

    creating theme and router provider

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,19 +1,10 @@
-import { Button } from "@/Components/shadcn/button";
 import { NotreonRoutes } from "./Routes/notreon";
 import { RouterProvider } from "react-router";
+import { ThemeProvider } from "./Theme/themeProvider";
 export default function App() {
   return (
-    <div className="p-2">
-      <h2>Notreon Project on development!.</h2>
-      <p>Steps left for frontend:</p>
-      <ul>
-        <li>2. backend</li>
-      </ul>
-      <Button variant="outline" onClick={() => alert("NO!")}>
-        Notify when ready
-      </Button>
-      <hr/>
+    <ThemeProvider>
       <RouterProvider router={NotreonRoutes} />
-    </div>
+    </ThemeProvider>
   );
 }
--- Frontend/src/Routes/notreon.tsx
+++ Frontend/src/Routes/notreon.tsx
@@ -2,14 +2,14 @@ import Layout from "@/Layout/Layout";
 import { createBrowserRouter } from "react-router";
 
 export const NotreonRoutes = createBrowserRouter([
- {
+  {
     path: "/",
     element: <Layout />,
     children: [
-        {
-            path: "/",
-            element: <h1>Home</h1>,
-        }
-    ]
- }
+      {
+        path: "/",
+        element: <h1>Home</h1>,
+      },
+    ],
+  },
 ]);
