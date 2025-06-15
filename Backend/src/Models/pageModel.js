fc9b962cd8f3cac6234343379e1ab1b52556ae0f
Vhmed <vhmdhoss@gmail.com>
2025-06-15 11:46:58 +0300

    deleting comment

--- Backend/src/Models/pageModel.js
+++ Backend/src/Models/pageModel.js
@@ -5,7 +5,7 @@ const pageSchema = new mongoose.Schema({
   content: { type: String, required: true, default: "" },
   title: { type: String, required: true, default: "New Page" },
   pageId: { type: String, required: true },
-  parentId: { type: mongoose.Schema.Types.ObjectId, ref: "Page", default: null }, // <-- this enables nesting
+  parentId: { type: mongoose.Schema.Types.ObjectId, ref: "Page", default: null }, 
   createdAt: {
     type: Date,
     default: Date.now,
