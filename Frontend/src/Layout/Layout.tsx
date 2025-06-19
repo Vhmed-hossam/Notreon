a92782c65261ee916dd88e4dba43264b8815bd0f
Vhmed <vhmdhoss@gmail.com>
2025-06-19 15:49:19 +0300

    starting navbar

--- Frontend/src/Layout/Layout.tsx
+++ Frontend/src/Layout/Layout.tsx
@@ -1,8 +1,10 @@
+import Navbar from "@/Components/Navbar/navbar";
 import { Outlet } from "react-router";
 
 export default function Layout() {
   return (
     <div>
+      <Navbar />
       <Outlet />
     </div>
   );
