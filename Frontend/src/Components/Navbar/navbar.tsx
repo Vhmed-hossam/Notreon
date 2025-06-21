f41f787bc46caff9afb4230f3e639e33b659b4e9
Vhmed <vhmdhoss@gmail.com>
2025-06-21 06:40:11 +0300

    finished unauh navbar

--- Frontend/src/Components/Navbar/navbar.tsx
+++ Frontend/src/Components/Navbar/navbar.tsx
@@ -1,7 +1,28 @@
+import { Link } from "react-router-dom";
+import { Button } from "../shadcn/button";
+
 export default function Navbar() {
   return (
-    <div className="navbar pt-4 px-4">
-      <div className="p-2">on UI/UX process</div>
+    <div className="pt-5 px-5">
+      <div className="p-3 navbar rounded-lg">
+        <div className="flex items-center p-2 justify-between gap-2">
+          <Link
+            className="flex gap-2 flex-row self-stretch flex-1 items-center"
+            to="/"
+          >
+            <img src="/Notreon_logo.png" alt="Notreon Logo" className="w-11" />
+            <h2 className="text-2xl confortaa">Notreon</h2>
+          </Link>
+          <div className="flex gap-2 flex-row self-stretch items-center">
+            <Link to="/login">
+              <Button variant="outline">Login</Button>
+            </Link>
+            <Link to="/register">
+              <Button>Register</Button>
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
