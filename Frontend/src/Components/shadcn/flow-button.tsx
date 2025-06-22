08df19c9c13b2b1aa8f88e22b9f50d59a98007a6
Vhmed <vhmdhoss@gmail.com>
2025-06-22 17:12:37 +0300

    adding select none to flow button

--- Frontend/src/Components/shadcn/flow-button.tsx
+++ Frontend/src/Components/shadcn/flow-button.tsx
@@ -2,7 +2,7 @@ import { ArrowRight } from "lucide-react";
 
 export function FlowButton({ text }: { text: string }) {
   return (
-    <button className="group size- relative flex items-center gap-1 overflow-hidden hover:text-white rounded-[100px] border-[1.5px] border-[#333333]/40 bg-transparent px-8 py-3 text-sm font-semibold cursor-pointer transition-all duration-[600ms] ease-[cubic-bezier(0.23,1,0.32,1)] hover:border-transparent hover:rounded-[12px] active:scale-[0.95]">
+    <button className="group select-none relative flex items-center gap-1 overflow-hidden hover:text-white rounded-[100px] border-[1.5px] border-[#333333]/40 bg-transparent px-8 py-3 text-sm font-semibold cursor-pointer transition-all duration-[600ms] ease-[cubic-bezier(0.23,1,0.32,1)] hover:border-transparent hover:rounded-[12px] active:scale-[0.95]">
       <ArrowRight className="absolute w-4 h-4 left-[-25%]  fill-none z-[9] group-hover:left-4 group-hover:stroke-white transition-all duration-[800ms] ease-[cubic-bezier(0.34,1.56,0.64,1)]" />
       <span className="relative z-[1] -translate-x-3 group-hover:translate-x-3 transition-all duration-[800ms] ease-out">
         {text}
